927f9cd3c64e29494f21c594c1a4c0cc6fa9401c
Ali NEDİM <81644230+AliNedim@users.noreply.github.com>
2021-04-26 18:13:43 +0300

    26 April
    26 April

--- panel/App/views/components/breadcrumb.php
+++ panel/App/views/components/breadcrumb.php
@@ -2,13 +2,8 @@
       <nav aria-label="breadcrumb">
           <ol class="breadcrumb Breadcrumb_Com" >
               <li class="breadcrumb-item" ><a href="<?=domain.dirnameProject.'/'.dirnameProjectHome?> ">Anasayfa</a></li>
-              <? $say = 1; for($sayi = 0; $sayi < count( explode( '/', @App::parseUrl() ) ); $sayi++): $say=$say+1; ?>
-
-
-                    <li class="breadcrumb-item" aria-current="page"><?=$sayi;?>.<?=$say;?></li>
-
-
-              <? endfor; ?>
+              <?php for($sayi = 0; $sayi < count( explode( '/', @App::parseUrl() ) ); $sayi++)
+                     echo '<li class="breadcrumb-item active" aria-current="page"><span><a href="#">'.@$data["{$sayi}"].'</a></li></span>';  ?>
           </ol>
       </nav>
 </div>
