cb2a0383dda451f1001fdd3ca2a3c2b055f0ee77
Ali NEDİM <81644230+AliNedim@users.noreply.github.com>
2021-05-09 21:57:44 +0300

    Delete panel directory

--- panel/App/views/components/breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<div class="page-info">
-      <nav aria-label="breadcrumb">
-          <ol class="breadcrumb Breadcrumb_Com" >
-              <li class="breadcrumb-item" ><a href="<?=domain.dirnameProject.'/'.dirnameProjectHome?> ">Anasayfa</a></li>
-              <?php for($sayi = 0; $sayi < count( explode( '/' , @App::parseUrl() ) ); $sayi++)
-                     echo '<li class="breadcrumb-item active" aria-current="page"><span>'.@$data["{$sayi}"].'</li></span>';  ?>
-          </ol>
-      </nav>
-</div>
